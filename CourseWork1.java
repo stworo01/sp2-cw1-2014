b2a2a248cd9d7ce50b076e4c05c07fe1ed153f51
stworo01 <stefan01@Stefans-Mac-mini.local>
2014-10-01 16:51:34 +0100

    arrays now stop filing when 0 is input

--- CourseWork1.java
+++ CourseWork1.java
@@ -16,18 +16,24 @@ public class CourseWork1 {
 		for(int i = 0; i < a.length; i++) {
 			System.out.println("Enter number for array 1");
 			a[i] = input.nextInt();
+            if(a[i] == 0) {
+                break;
+            }
 			
 		}
 		//fill array b
 		for(int i = 0; i < a.length; i++) {
 			System.out.println("Enter number for array 2");
 			b[i] = input.nextInt();
+            if(b[i] == 0) {
+                break;
+            }
+
 	}
 		// print the arrays
 		
 		
 		System.out.println("The contents of array 1 are: "+ Arrays.toString(a));
-//		
 		System.out.println();
 
 		System.out.println("The contents of array 2 are: "+ Arrays.toString(b));
