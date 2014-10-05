cf2c3aa747a3c9b1ed4d6154b4df35261a89048c
stworo01 <stefan_1@Stefan-beast.local>
2014-10-05 10:33:38 +0100

    Revert "Nearly There"
    This reverts commit c4a2a820ed5d28c3cd52651a4e3df847e439cf27.

--- CourseWork1.java
+++ CourseWork1.java
@@ -13,144 +13,62 @@
 
 */
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class CourseWork1 {
     // variables
-    final static int LENGTH = 100;
+    final static int LENGTH = 4;
     static int[] a = new int[LENGTH];
 	static int[] b = new int[LENGTH];
 
-
     public static void main(String[] args) {
         readInputs();
-     
-        
-        
-      
+        dup();
     }// end main
     /** 
-        Takes user inputs for two arrays, prints out the two arrays
+        Takes user inputs for two arrays, prints out the twom arrays
     */
     public static void readInputs() {
-        Scanner input = new Scanner(System.in);
-        //variables to keep fill size of arrays
-        int aSize;
-        int bSize;
-        // fill array a     
-		for(aSize = 0; aSize < a.length; aSize++) {
+                Scanner input = new Scanner(System.in);
+        // fill array a
+		for(int i = 0; i < a.length; i++) {
 			System.out.println("Enter number for array 1");
-			a[aSize] = input.nextInt();
-            if(a[aSize] == 0) {
-            	break;
-                
+			a[i] = input.nextInt();
+            if(a[i] == 0) {
+                break;
             }// end if
 		} //end for
 		//fill array b
-		for(bSize = 0; bSize < b.length; bSize++) {
+		for(int i = 0; i < a.length; i++) {
 			System.out.println("Enter number for array 2");
-			b[bSize] = input.nextInt();
-            if(b[bSize] == 0) {
-             
-               
+			b[i] = input.nextInt();
+            if(b[i] == 0) {
                 break;
             }// end if
         }// end for
-		//print the elements of the arrays
-		if(aSize == 0 && bSize == 0) { 
-			System.out.println("Both arrays are empty");
-	      
-
-		
-		} else {
-			System.out.print("The elements of Array 1 are: ");
-			for(int i = 0; i < aSize; i++ ) {
-				System.out.print(" "+a[i]);
-			} //end for
-			System.out.println();
-		
-			System.out.print("The elements of Array 2 are: ");
-			for(int i = 0; i < bSize; i++) {
-				System.out.print(" "+b[i]);
-			} //end for
-			System.out.println();
-			  dup();
-		      nDup();
-		}
+        System.out.println("The contents of array 1 are: "+ Arrays.toString(a));
+		System.out.println();
+		System.out.println("The contents of array 2 are: "+ Arrays.toString(b));
         input.close();
-      
-   
     }// end readInputs
-    
-    
-    
-    
-    
-    
-    
-    
-  
     /**
         Prints out the duplicated numbers the two arrays
     */
     public static void dup() {
         int count = 0;
-        System.out.print("The numbers that are duplicated are: ");
+        System.out.print("duplicates are: ");
          for(int i = 0; i < a.length; i ++) {
-            for (int j = 0; j < b.length; j++) {  	
-            	if(a[i] == 0) { break;} //prevent zero's being printed
+            for (int j = 0; j < b.length; j++) {
                 if(a[i] == b[j]) {
                     int dup = a[i];
                     count++;
                     System.out.print(dup+ " ");
-                } 
+                } // end if
             } //end for
         }// end for
         System.out.println();
-        System.out.println("There are " + count+" duplicates");     
+        System.out.println("There are " + count);
     } //end dup
-    
-    
-    /** prints out the elements that are not duplicated in the arrays
-     * 
-     */
-    public static void nDup() {
-    	int count;
-    	System.out.print("The elements that are in A but not B are: ");
-    	for(int i = 0; i < a.length; i++) {
-    		count = 0;
-    		for(int j = 0; j < b.length; j++) {
-    			if(a[i] == 0) { break;}
-    			if(a[i] != b[j]) {
-    				count++;
-    				if(count == a.length ) { 
-    					int nDup = a[i];
-    					System.out.print(nDup+ " ");
-    				}
-    			}
-    		}
-    		
-    		
-    	}
-    	System.out.println();
-    	System.out.print("The elements that are in B but not A are: ");
-    	
-    	for(int i = 0; i < b.length; i++) {
-    		count = 0;
-    		for(int j = 0; j < b.length; j++) {
-    			if(b[i] == 0) { break;}
-    			if(b[i] != a[j]) {
-    				count++;
-    				if(count == b.length ) { 
-    					
-    					int nDup = b[i];
-    					System.out.print(nDup+ " ");
-    				}
-    			}
-    		}
-    		
-    		
-    	}
-    }
+
+
 }// end class
-
-
